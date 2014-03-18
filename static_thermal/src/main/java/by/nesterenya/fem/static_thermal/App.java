5e1b094987baf6bc21fbce99219aad9624c60b91
Igor Nesterenya <nesterione@gmail.com>
2014-03-18 19:01:38 +0300

    Create base of project

---
+++ static_thermal/src/main/java/by/nesterenya/fem/static_thermal/App.java
@@ -0,0 +1,13 @@
+package by.nesterenya.fem.static_thermal;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
